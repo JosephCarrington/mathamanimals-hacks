854eba8b3228dca3afa438478bf96851891613d9
Joseph Carrington <joseph.carrington@gmail.com>
2017-12-16 15:56:46 -0700

    Initial Commit with barebones UI

---
+++ Plugins/FlipFlopButtonController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlipFlopButtonController : MonoBehaviour {
+
+	private bool flipFlop = false;
+	private Text text;
+	// Use this for initialization
+	void Start () {
+		text = gameObject.GetComponent<Text> ();
+		CreateString ();
+
+	}
+
+	public void ToggleFlipFlop() {
+		flipFlop = !flipFlop;
+		GameObject.Find ("0centerField").SendMessage ("ToggleFlipFlopFun");
+		CreateString ();
+	}
+
+	private void CreateString() {
+		text.text = "FLIP FLOP FUN: ";
+		if (flipFlop) {
+			text.text += "ON";
+		} else {
+			text.text += "OFF";
+		}
+	}
+}
---
+++ myAssets/myScripts/Globals.js
@@ -0,0 +1,16 @@
+#pragma strict
+
+static var KillCards : boolean = false;
+static var FlipFlopFun : boolean = false;
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function ToggleFlipFlopFun() {
+	this.FlipFlopFun = !FlipFlopFun;
+}
