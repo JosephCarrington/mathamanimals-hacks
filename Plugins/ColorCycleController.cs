d514a70813042242e54008cda5b47cf4cc4381d2
Joseph Carrington <joseph.carrington@gmail.com>
2017-12-17 20:26:45 -0700

    Added tut

---
+++ Plugins/ColorCycleController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Mathamanimals {
+	public class ColorCycleController : MonoBehaviour {
+
+		// Use this for initialization
+		public Color[] colorsToCycle;
+		Color initialColor;
+		public bool lerpColors = true;
+		public float cycleTime = 1f;
+		Text text;
+		int currentColor = 1;
+		int oldColor = 0;
+
+		void Start () {
+			text = gameObject.GetComponent<Text> ();
+			initialColor = text.color;
+		}
+
+		void Cycle(bool cycle) {
+			StopAllCoroutines ();
+			if (cycle) {
+				StartCoroutine (CycleColors ());
+			} else {
+				text.color = initialColor;
+			}
+		}
+
+		IEnumerator CycleColors() {
+			while (true) {
+				if (lerpColors) {
+					StartCoroutine(LerpFromColorToColor (colorsToCycle [oldColor], colorsToCycle [currentColor], cycleTime));
+				} else {
+					text.color = colorsToCycle [oldColor];
+				}
+				oldColor++;
+				if (oldColor == colorsToCycle.Length) {
+					oldColor = 0;
+				}
+				currentColor++;
+				if (currentColor == colorsToCycle.Length) {
+					currentColor = 0;
+				}
+				yield return new WaitForSeconds (cycleTime);
+			}
+		}
+
+		IEnumerator LerpFromColorToColor(Color a, Color b, float length) {
+			float startTime = Time.time;
+			while (Time.time < startTime + length) {
+				float factor = (Time.time - startTime) / ((startTime + length) - startTime);
+				text.color = Color.Lerp(a, b, factor);
+				yield return null;
+			}
+		}
+		// Update is called once per frame
+		void Update () {
+			
+		}
+	}
+}
--- Plugins/FlipFlopButtonController.cs
+++ Plugins/FlipFlopButtonController.cs
@@ -3,29 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class FlipFlopButtonController : MonoBehaviour {
+namespace Mathamanimals {
+	public class FlipFlopButtonController : MonoBehaviour {
 
-	private bool flipFlop = false;
-	private Text text;
-	// Use this for initialization
-	void Start () {
-		text = gameObject.GetComponent<Text> ();
-		CreateString ();
+		private bool flipFlop = false;
+		private Text text;
+		public GameObject flipFlopTut;
+		public GameObject[] deactiveOnFlipFlopOn;
 
-	}
+		// Use this for initialization
+		void Start () {
+			text = gameObject.GetComponent<Text> ();
+			CreateString ();
 
-	public void ToggleFlipFlop() {
-		flipFlop = !flipFlop;
-		GameObject.Find ("0centerField").SendMessage ("ToggleFlipFlopFun");
-		CreateString ();
-	}
+		}
 
-	private void CreateString() {
-		text.text = "FLIP FLOP FUN: ";
-		if (flipFlop) {
-			text.text += "ON";
-		} else {
-			text.text += "OFF";
+		public void ToggleFlipFlop() {
+			flipFlop = !flipFlop;
+			GameObject.Find ("0centerField").SendMessage ("ToggleFlipFlopFun");
+			CreateString ();
+			if (flipFlop) {
+				SendMessage ("Cycle", true);
+				flipFlopTut.SetActive (true);
+				foreach (GameObject deactive in deactiveOnFlipFlopOn) {
+					deactive.SetActive (false);
+				}
+			} else {
+				SendMessage ("Cycle", false);
+				GetComponent<ColorCycleController> ().enabled = false;
+			}
+		}
+
+		private void CreateString() {
+			text.text = "FLIP FLOP FUN: ";
+			if (flipFlop) {
+				text.text += "ON";
+			} else {
+				text.text += "OFF";
+			}
 		}
 	}
 }
